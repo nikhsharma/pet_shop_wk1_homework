36aeef669f015d99598211825bea53be82d7899c
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-03-30 13:53:27 +0100

    17 passing tests

--- pet_shop.rb
+++ pet_shop.rb
@@ -56,3 +56,7 @@ end
 def add_pet_to_customer(customer, new_pet)
   return customer[:pets].push(new_pet)
 end
+
+def customer_can_afford_pet(customer, new_pet)
+  customer[:cash] < new_pet[:price] ? false : true
+end
