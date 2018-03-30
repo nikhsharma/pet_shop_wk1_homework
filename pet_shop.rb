1475c2ce5078161f3f11dd5fbe008d2d61aa6cd3
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-03-30 13:04:02 +0100

    nine passed tests

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,14 @@ end
 def stock_count(pet_shop)
   pet_shop[:pets].count
 end
+
+def pets_by_breed(pet_shop, breed)
+  number_of_pets_by_breed = []
+  pets = pet_shop[:pets]
+  for pet in pets
+    if pet[:breed] == breed
+      number_of_pets_by_breed.push(pet)
+    end
+  end
+  return number_of_pets_by_breed
+end
